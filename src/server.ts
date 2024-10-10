bb0147ab6196da54b16bb0f16710c6cb43348068
Karolayne Silva <96891482+Karolayne-silva@users.noreply.github.com>
2024-10-10 11:34:37 -0300

    criando rotas de usuários

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,14 @@ import express from "express";
 import connectDB from "./db/database";
 import TaskRouter from "./routes/TaskRoutes";
 import TagsRouter from "./routes/TagsRoutes";
+import UserRouter from "./routes/UsersRoutes";
 
 const app = express();
 app.use(express.json());
 
-app.use('/tasks', TaskRouter);
-app.use('/tags', TagsRouter);
+app.use("/tasks", TaskRouter);
+app.use("/tags", TagsRouter);
+app.use("/users", UserRouter);
 
 connectDB();
 
